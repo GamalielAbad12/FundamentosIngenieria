5eafc4efd8ee90032a175fa6d6a6cb83a07498ce
Gamaliel Abad <gamalielabad@gmail.com>
2024-01-15 13:32:59 -0600

    Hola

---
+++ ActaMedica.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int edad;
+    char sexo[10];
+    float estatura;
+
+    cout << "Ingresa tu edad: "; cin >> edad;
+    cout << "\nIngresa tu sexo: "; cin >> sexo;
+    cout << "\nIngresa tu estatura: "; cin >> estatura;
+
+    cout << "Tu edad: " << edad << endl;
+    cout << "Tu sexo: " << sexo << endl;
+    cout << "Tu estatura: " << estatura << endl;
+
+    return 0;
+
+
+
+}
